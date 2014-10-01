fe0f8d317009e289840e932fd6aafc1e8a5c96ff
unknown <sstokic@student.tgm.ac.at>
2014-10-01 15:33:17 +0200

    Lagermitarbeiter finalisiert. Kommentare fehlen

--- src/stokic_kodras_elfar/Lagermitarbeiter.java
+++ src/stokic_kodras_elfar/Lagermitarbeiter.java
@@ -1,21 +1,20 @@
 package stokic_kodras_elfar;
 
 import java.io.*;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.logging.*;
 
-import javax.swing.JOptionPane;
 
 /**
  * Klasse welche neue Teile hinzufügt, löscht bzw. eine Klasse die die Teile verwaltet
  * Darunter versteht man ebenfalls, das Speichern/Loeschen der Teile in eine .csv Datei
  * 
  * @author Stokic Stefan
- * @version 1.0
+ * @version 1.6
  */
 public class Lagermitarbeiter implements Runnable, Stoppable {
 
-	private int id;
 	private String logV;
 	private String lagerV;
 	private boolean isRunning;
@@ -66,20 +65,35 @@ public class Lagermitarbeiter implements Runnable, Stoppable {
 
 			}catch(IOException ioe){
 
-				JOptionPane.showMessageDialog(null, "IOException: " + ioe.getMessage());
+				log.log(Level.SEVERE, "IOException: " + ioe.getMessage());
 			}
 		}
 
-		this.auge = new ConcurrentLinkedQueue<String>();
-		this.arm = new ConcurrentLinkedQueue<String>();
-		this.kettenantrieb = new ConcurrentLinkedQueue<String>();
-		this.rumpf = new ConcurrentLinkedQueue<String>();
-		this.tmp = new ConcurrentLinkedQueue<String>();
+		auge = new ConcurrentLinkedQueue<String>();
+		arm = new ConcurrentLinkedQueue<String>();
+		kettenantrieb = new ConcurrentLinkedQueue<String>();
+		rumpf = new ConcurrentLinkedQueue<String>();
+		tmp = new ConcurrentLinkedQueue<String>();
+
+		this.isRunning = true;
 	}
 
 	@Override
 	public void run(){
 
+		while(this.isRunning) {
+
+			apply();
+
+			try {
+
+				Thread.sleep(500);
+
+			}catch(InterruptedException ie) {
+
+				log.log(Level.SEVERE, "InterruptedException: " + ie.getMessage());
+			}
+		}
 	}
 
 	@Override
@@ -190,15 +204,151 @@ public class Lagermitarbeiter implements Runnable, Stoppable {
 
 		return part;
 	}
-	
+
 	/**
 	 * Teil aus dem Lager entfernen
 	 * 
 	 * @param part
 	 */
 	public void deletePart(String part) {
-		
+
 		tmp.remove(part);
 		log.log(Level.INFO, part + " aus dem Lager entfernt!");
+	}
+
+	/**
+	 * Teil in das Lager zuruecklegen
+	 * 
+	 * @param parts
+	 * @param part
+	 */
+	public void returnPart(RoboParts parts, String part) {
+
+		tmp.remove(part);
+
+		switch(parts) {
+
+		case AUGE:
+
+			auge.add(part);
+			log.log(Level.INFO, part + " in das Lager zurueckgelegt!");
+			break;
+
+		case ARM:
+
+			arm.add(part);
+			log.log(Level.INFO, part + " in das Lager zurueckgelegt!");
+			break;
+
+		case KETTENANTRIEB:
+
+			kettenantrieb.add(part);
+			log.log(Level.INFO, part + " in das Lager zurueckgelegt!");
+			break;
+
+		case RUMPF:
+
+			rumpf.add(part);
+			log.log(Level.INFO, part + " in das Lager zurueckgelegt");
+			break;
+		}
+	}
+
+	/**
+	 * Roboter parts in eine Datei schreiben
+	 * 
+	 * @param path
+	 * @param data
+	 * @return
+	 */
+	public boolean writeToFile(String path, ConcurrentLinkedQueue<String> data) {
+
+		try {
+
+			String out = "";
+
+			File f = new File(path);
+			if(!f.exists()){
+
+				f.createNewFile();
+
+			}else{
+
+				f.delete();
+			}
+
+			FileWriter fw = new FileWriter(f.getAbsoluteFile(), true);
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			for(String lines : data) {
+
+				lines += System.getProperty("line.seperator");
+				out += lines;
+			}
+
+			bw.write(out);
+			bw.close();
+
+			return true;
+
+		}catch(IOException ioe) {
+
+			log.log(Level.SEVERE, "IOException: " + ioe.getMessage());
+			return false;
+		}
+	}
+
+	/**
+	 * Roboter parts aus einer Datei lesen
+	 * 
+	 * @param path
+	 * @return
+	 */
+	public LinkedList<String> loadFromFile(String path) {
+
+		try {
+
+			File f = new File(path);
+			if(!f.exists()){
+
+				f.createNewFile();
+
+			}else{
+
+				f.delete();
+			}
+
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr);
+
+			LinkedList<String> list = new LinkedList<String>();
+
+			String lines = null;
+
+			while((lines = br.readLine()) != null) {
+
+				list.add(lines);
+			}
+
+			br.close();
+
+			return list;
+
+		}catch(IOException ioe) {
+
+			log.log(Level.SEVERE, "IOException: " + ioe.getMessage());
+			return null;
+		}
+	}
+
+	/**
+	 * Roboter parts in .csv Dateien schreiben
+	 */
+	private void apply() {
+
+		writeToFile(this.lagerV + "/augen.csv", auge);
+		writeToFile(this.lagerV + "/arm.csv", arm);
+		writeToFile(this.lagerV + "/kettenantrieb.csv", kettenantrieb);
+		writeToFile(this.lagerV + "/rumpf.csv", rumpf);
 	}
 }
