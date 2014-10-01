13a66668ee810d6c490ceea1fc7e220f0d8f8c9c
Matthias El-Far <matthias.elfar@mail.tgm.ac.at>
2014-10-01 19:30:06 +0200

    monteur Teile sammlung implementiert

--- src/stokic_kodras_elfar/Montuer.java
+++ src/stokic_kodras_elfar/Montuer.java
@@ -72,8 +72,7 @@ public class Montuer implements Runnable, Stoppable {
 
 	@Override
 	public void run(){
-
-		//	sort();
+		
 		buildRobot();
 	}
 
@@ -112,7 +111,7 @@ public class Montuer implements Runnable, Stoppable {
 		}
 
 		tmpArmL = lagerMit.getPart(RoboParts.ARM);
-		log.log(Level.INFO, "Hole linkes Arm aus dem Lager!");
+		log.log(Level.INFO, "Hole linken Arm aus dem Lager!");
 		if(tmpArmL == null) {
 
 			log.log(Level.WARNING, "Kein linkes Arm vorhanden, ich lege zurueck!");
@@ -120,6 +119,40 @@ public class Montuer implements Runnable, Stoppable {
 			lagerMit.returnPart(RoboParts.AUGE, tmpAugeR);
 			return;
 		}
+		tmpArmR = lagerMit.getPart(RoboParts.ARM);
+		log.log(Level.INFO, "Hole rechten Arm aus dem Lager!");
+		if(tmpArmR == null) {
+
+			log.log(Level.WARNING, "Kein rechter Arm vorhanden, ich lege zurueck!");
+			lagerMit.returnPart(RoboParts.AUGE, tmpAugeL);
+			lagerMit.returnPart(RoboParts.AUGE, tmpAugeR);
+			lagerMit.returnPart(RoboParts.ARM, tmpArmL);
+			return;
+		}
+		tmpRumpf = lagerMit.getPart(RoboParts.RUMPF);
+		log.log(Level.INFO, "Hole Rumpf aus dem Lager!");
+		if(tmpRumpf == null) {
+
+			log.log(Level.WARNING, "Kein Rumpf vorhanden, ich lege zurueck!");
+			lagerMit.returnPart(RoboParts.AUGE, tmpAugeL);
+			lagerMit.returnPart(RoboParts.AUGE, tmpAugeR);
+			lagerMit.returnPart(RoboParts.ARM, tmpArmL);
+			lagerMit.returnPart(RoboParts.ARM, tmpArmR);
+			return;
+		}
+		tmpKettenantrieb = lagerMit.getPart(RoboParts.KETTENANTRIEB);
+		log.log(Level.INFO, "Hole Kettenantrieb aus dem Lager!");
+		if(tmpKettenantrieb == null) {
+
+			log.log(Level.WARNING, "Kein Kettenantrieb vorhanden, ich lege zurueck!");
+			lagerMit.returnPart(RoboParts.AUGE, tmpAugeL);
+			lagerMit.returnPart(RoboParts.AUGE, tmpAugeR);
+			lagerMit.returnPart(RoboParts.ARM, tmpArmL);
+			lagerMit.returnPart(RoboParts.ARM, tmpArmR);
+			lagerMit.returnPart(RoboParts.RUMPF, tmpRumpf);
+			return;
+		}
+
 
 		// usw. wird immer um das vorherige groesser
 
@@ -128,12 +161,20 @@ public class Montuer implements Runnable, Stoppable {
 		augeL = tmpAugeL.split(",");
 		augeR = tmpAugeR.split(",");
 		armL = tmpArmL.split(",");
+		armR = tmpArmR.split(",");
+		rumpf = tmpRumpf.split(",");
+		kettenantrieb = tmpKettenantrieb.split(",");
+		 
 		// usw...
 
 		// dann fuer das sortierte vorbereiten
 		this.augeL = new int[augeL.length - 1];
 		this.augeR = new int[augeR.length - 1];
 		this.armL = new int[armL.length - 1];
+		this.armR = new int[armR.length - 1];
+		this.rumpf = new int[rumpf.length - 1];
+		this.kettenantrieb = new int[kettenantrieb.length - 1];
+		
 		// usw...
 
 		// befuellen
@@ -151,12 +192,31 @@ public class Montuer implements Runnable, Stoppable {
 
 			this.armL[i] = Integer.parseInt(armL[i]);
 		}
+		
+		for(int i = 0; i < this.armR.length; i++) {
+
+			this.armR[i] = Integer.parseInt(armR[i]);
+		}
+		
+		for(int i = 0; i < this.rumpf.length; i++) {
+
+			this.rumpf[i] = Integer.parseInt(rumpf[i]);
+		}
+
+		for(int i = 0; i < this.kettenantrieb.length; i++) {
+
+			this.kettenantrieb[i] = Integer.parseInt(kettenantrieb[i]);
+		}
 		// usw...
 		
 		// die teile arrays sorten
 		Arrays.sort(this.augeL);
 		Arrays.sort(this.augeR);
 		Arrays.sort(this.armL);
+		Arrays.sort(this.armR);
+		Arrays.sort(this.rumpf);
+		Arrays.sort(this.kettenantrieb);
+		
 		// usw...
 		
 		String out = "Threadee-ID" + /* threadee-id */ ",Mitarbeiter-ID" + this.id + ",";
@@ -181,10 +241,55 @@ public class Montuer implements Runnable, Stoppable {
 				augeRfinal += ",";
 			}
 		}
+		
+		armLfinal = "Arm,";
+		for(int i = 0; i < this.armL.length; i++) {
+			
+			armLfinal += this.armL[i];
+			if(i != (this.armL.length - 1)) {
+				
+				armLfinal += ",";
+			}
+		}
+		
+		armRfinal = "Arm,";
+		for(int i = 0; i < this.armR.length; i++) {
+			
+			armRfinal += this.armR[i];
+			if(i != (this.armR.length - 1)) {
+				
+				armRfinal += ",";
+			}
+		}
+		
+		rumpffinal = "Rumpf,";
+		for(int i = 0; i < this.rumpf.length; i++) {
+			
+			rumpffinal += this.rumpf[i];
+			if(i != (this.rumpf.length - 1)) {
+				
+				rumpffinal += ",";
+			}
+		}
+		
+		kettenantriebfinal = "Kettenantrieb,";
+		for(int i = 0; i < this.kettenantrieb.length; i++) {
+			
+			kettenantriebfinal += this.kettenantrieb[i];
+			if(i != (this.kettenantrieb.length - 1)) {
+				
+				kettenantriebfinal += ",";
+			}
+		}
 		// usw...
 		
 		out += augeLfinal + ",";
 		out += augeRfinal + ",";
+		out += armLfinal + ",";
+		out += armRfinal + ",";
+		out += rumpffinal + ",";
+		out += kettenantrieb;
+		
 		// usw...
 		// beim letzten element braucht man kein "," extra
 		
